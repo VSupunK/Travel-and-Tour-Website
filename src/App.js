dfb68bb3c4fd88c812153d0010b32d07c2cb81cb
VSK <vsupunkalhara@gmail..com>
2024-04-07 01:16:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './app.css'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/Home/Home'
+import Main from './Components/Main/Main'
+import Navbar from './Components/Navbar/Navbar'
+
+
+export const App = () => {
+  return (
+    <>
+    <Navbar/>
+    {/*<Home/>
+    <Main/>
+    <Footer/>*/}
+  </>
+  )
+}
+
+export default App
